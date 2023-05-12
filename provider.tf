41029619360915cded3df919c8bd58b963e92023
Sathvik k k <50857156+sathvikkk@users.noreply.github.com>
2023-05-12 10:24:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "google" {
-  credentials = "${file("sinuous-transit-377210-fed835d25221.json")}"
-  project = "sinuous-transit-377210"
-  region = "us-central1"
-  zone = "us-central1-a"
+  credentials = "${file("")}"
+  project = ""
+  region = ""
+  zone = ""
 
 }
